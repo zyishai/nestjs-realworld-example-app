c5861d427aafbd6bfc9745ce3eafc3b5550f59a3
Yishai Zehavi <zehaviyishai@gmail.com>
2020-07-08 21:00:31 +0300

    adjust to cloud lab project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,33 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { ArticleModule } from './article/article.module';
-import { UserModule } from './user/user.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Connection } from 'typeorm';
-import { ProfileModule } from './profile/profile.module';
-import { TagModule } from './tag/tag.module';
+import {Module} from '@nestjs/common';
+import {ConfigModule} from '@nestjs/config';
+import {AppController} from './app.controller';
+import {ArticleModule} from './article/article.module';
+import {UserModule} from './user/user.module';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {Connection} from 'typeorm';
+import {ProfileModule} from './profile/profile.module';
+import {TagModule} from './tag/tag.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: 'nestjsrealworld',
+      entities: ['src/**/**.entity{.ts,.js}'],
+      synchronize: true,
+    }),
     ArticleModule,
     UserModule,
     ProfileModule,
-    TagModule
+    TagModule,
   ],
-  controllers: [
-    AppController
-  ],
-  providers: []
+  controllers: [AppController],
+  providers: [],
 })
 export class ApplicationModule {
   constructor(private readonly connection: Connection) {}
